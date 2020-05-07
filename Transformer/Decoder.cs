2c7996c0af0301024e7f2cfd663436c78fed7e40
Andy Manoske <andy@hashicorp.com>
2020-05-07 14:16:47 -0700

    Update Decoder.cs

--- Transformer/Decoder.cs
+++ Transformer/Decoder.cs
@@ -1,10 +1,24 @@
-﻿using System;
+using System;
+using VaultSharp.V1.Commons;
+using VaultSharp.V1.SecretsEngines.Transform;
+
 namespace Transformer
 {
     public class Decoder
     {
-        public Decoder()
+        public Decoder(Connection client)
         {
+            connection = client;
         }
+
+        public async System.Threading.Tasks.Task<string> decode(String decode_value, String role_name)
+        {
+            var decodeOptions = new DecodeRequestOptions { Value = decode_value};
+            Secret<DecodedResponse> response = await connection.session.V1.Secrets.Transit.DecryptAsync(role_name, decodeOptions);
+            return response.Data.DecodedValue;
+        }
+
+        Connection connection;
+
     }
 }
