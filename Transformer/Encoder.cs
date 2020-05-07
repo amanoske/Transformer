ff9d51e0c3a35bd0cc35adf897a69f0b41ed5ad7
Andy Manoske <andy@hashicorp.com>
2020-05-07 14:17:14 -0700

    Update Encoder.cs

--- Transformer/Encoder.cs
+++ Transformer/Encoder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using VaultSharp.V1.Commons;
 using VaultSharp.V1.SecretsEngines.Transform;
 
@@ -18,7 +18,13 @@ namespace Transformer
             return response.Data.EncodedValue.ToString();
         }
 
-    
+        public async System.Threading.Tasks.Task<string> encode(String encode_value, String role_name, String endpoint_path)
+        {
+            var encodeOptions = new EncodeRequestOptions { Value = encode_value };
+            Secret<EncodedResponse> response = await connection.session.V1.Secrets.Transit.EncryptAsync(encode_value, encodeOptions, endpoint_path);
+            return response.Data.EncodedValue.ToString();
+        }
+
         Connection connection;
     }
 }
